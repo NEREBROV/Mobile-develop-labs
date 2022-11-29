55e796d90d5527cac13fdec6310e4b0bf1c68756
NEREBROV <riebrovn2000@gmail.com>
2022-11-29 16:12:28 +0700

    Листинг 11.2. Превращаем Crime в сущность (Crime.kt) Entity - красное PrimaryKey - красное

--- CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/Crime.kt
@@ -2,8 +2,8 @@ package com.example.criminalintent
 
 import java.util.*
 //import java.util.Date
-
-data class Crime(val id: UUID = UUID.randomUUID(),
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(),
                  var title: String = "",
                  var date: Date = Date(),
                  var isSolved: Boolean = false)
