5a1025e121576b478ba02da09731933be9496177
NEREBROV <riebrovn2000@gmail.com>
2022-11-29 14:45:13 +0700

    Листинг 9.10. Извлечение представления в конструктор (CrimeListFragment.kt)

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,6 +34,8 @@ class CrimeListFragment : Fragment() {
     }
     private inner class CrimeHolder(view: View)
         : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
     }
     companion object {
         fun newInstance(): CrimeListFragment {
