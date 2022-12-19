b1e157ca838b2ab6b90be56beba9389d4f870929
NEREBROV <riebrovn2000@gmail.com>
2022-12-19 17:41:06 +0700

    Листинг 13.2. Отображение DialogFragment (CrimeFragment.kt)
    стр 435 - как свзязать? (к 13.1)

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -17,6 +17,8 @@ import java.util.*
 
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
+private const val DIALOG_DATE = "DialogDate"
+
 
 class CrimeFragment : Fragment() {
 
@@ -44,10 +46,7 @@ class CrimeFragment : Fragment() {
         dateButton = view.findViewById(R.id.crime_date) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
 
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-        }
+
         return view
     }
 
@@ -96,6 +95,13 @@ class CrimeFragment : Fragment() {
                 crime.isSolved = isChecked
             }
         }
+
+        dateButton.setOnClickListener {
+            DatePickerFragment().apply {
+                show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
+            }
+        }
+
     }
 
 
