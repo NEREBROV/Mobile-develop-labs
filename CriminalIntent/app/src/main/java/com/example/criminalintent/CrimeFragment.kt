babd4dfb0ff4e75979e0753270e9b2b16667b963
NEREBROV <riebrovn2000@gmail.com>
2022-12-19 17:14:12 +0700

    Листинг 12.16. Сохранение в onStop() (CrimeFragment.kt)

--- CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -98,6 +98,13 @@ class CrimeFragment : Fragment() {
         }
     }
 
+
+    override fun onStop() {
+        super.onStop()
+        crimeDetailViewModel.saveCrime(crime)
+    }
+
+
     private fun updateUI() {
         titleField.setText(crime.title)
         dateButton.text = crime.date.toString()
